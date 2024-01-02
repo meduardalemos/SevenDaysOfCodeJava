94c77818ac516cb33cf9f54bf9832b1dda7873aa
meduardalemos <maria.eduarda.lemos@hotmail.com>
2024-01-02 11:26:41 -0300

    Increase query results limit from 20 to 250

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,72 +21,64 @@ public class Main {
         }
 
         // Requisição a API
-        String responseBody = null;
+        HttpClient client = HttpClient.newHttpClient();
+        // String responseBody = null;
 
-        try {
-            URI uri = new URI("https://api.themoviedb.org/3/movie/top_rated?api_key=" + apiKey);
+        // Variáveis para controle da quantidade de resultados obtidos
+        int desiredResults = 250;
+        int totalResults = 0;
+        int currentPage = 1;
 
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(uri)
-                    .GET()
-                    .build();
-            HttpResponse<String> response = client.send(request,
-                    HttpResponse.BodyHandlers.ofString());
+        // Cria listas para armazenar títulos e caminho dos posters
+        List<String> titles = new ArrayList<>();
+        List<String> posterPaths = new ArrayList<>();
+        List<Integer> releaseYears = new ArrayList<>();
+        List<Double> voteAverages = new ArrayList<>();
 
-            responseBody = response.body();
-        } catch (Exception e) {
-            System.err.println("Ocorreu um erro ao tentar acessar a API: " + e.getMessage());
-            e.printStackTrace();
-        }
+        do {
+            try {
+                URI uri = new URI("https://api.themoviedb.org/3/movie/top_rated?api_key=" +
+                        apiKey + "&page=" + currentPage);
 
-        // Verifica se a resposta não está vazia antes de tentar processar o Json
-        if (responseBody != null && !responseBody.isEmpty()) {
-            // Converte a resposta em um objeto JSON
-            JSONObject json = new JSONObject(responseBody);
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(uri)
+                        .GET()
+                        .build();
 
-            // Obtem a matriz "results" do JSON
-            JSONArray resultsArray = json.getJSONArray("results");
+                HttpResponse<String> response = client.send(request,
+                        HttpResponse.BodyHandlers.ofString());
 
-            // Cria listas para armazenar títulos e caminho dos posters
-            List<String> titles = new ArrayList<>();
-            List<String> posterPaths = new ArrayList<>();
-            List<Integer> releaseYears = new ArrayList<>();
-            List<Double> voteAverages = new ArrayList<>();
+                JSONObject json = new JSONObject(response.body());
+                JSONArray resultsArray = json.getJSONArray("results");
 
-            // Itera sobre os 250 primeiros 250 filmes ou até o final da matriz
-            int limit = Math.min(resultsArray.length(), 250);
-            for (int i = 0; i < limit; i++) {
-                // Obtém o objeto JSON do filme atual
-                JSONObject movie = resultsArray.getJSONObject(i);
+                totalResults += resultsArray.length();
 
-                // Obtém o título e caminho do poster do filme atual
-                String title = movie.getString("title");
-                String posterPath = movie.getString("poster_path");
-                Integer releaseYear = Integer.parseInt(movie.getString("release_date").substring(0,
-                        4));
-                Double voteAverage = movie.getDouble("vote_average");
+                for (int i = 0; i < resultsArray.length(); i++) {
 
-                // Adiciona o titulo e caminho do poster às listas respectivas
-                titles.add(title);
-                posterPaths.add(posterPath);
-                releaseYears.add(releaseYear);
-                voteAverages.add(voteAverage);
+                    if(titles.size() >= desiredResults) {
+                        break;
+                    }
+
+                    JSONObject movie = resultsArray.getJSONObject(i);
+
+                    String title = movie.getString("title");
+                    String posterPath = movie.getString("poster_path");
+                    Integer releaseYear = Integer.parseInt(movie.getString("release_date")
+                            .substring(0, 4));
+                    Double voteAverage = movie.getDouble("vote_average");
+
+                    titles.add(title);
+                    posterPaths.add(posterPath);
+                    releaseYears.add(releaseYear);
+                    voteAverages.add(voteAverage);
+                }
+
+                currentPage++;
+
+            } catch (Exception e) {
+                System.err.println("Ocorreu um erro ao tentar acessar a API: " + e.getMessage());
+                e.printStackTrace();
             }
-
-            System.out.println("Lista de titulos: ");
-            titles.forEach(System.out::println);
-
-            System.out.println("\nLista de caminhos dos posteres: ");
-            posterPaths.forEach(System.out::println);
-
-            System.out.println("\nLista de anos de lancamento: ");
-            releaseYears.forEach(System.out::println);
-
-            System.out.println("\nLista de medias de notas: ");
-            voteAverages.forEach(System.out::println);
-        } else {
-            System.out.println("A resposta da API está vazia.");
-        }
+        } while (titles.size() < desiredResults);
     }
 }
